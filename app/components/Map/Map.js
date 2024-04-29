bf82b31657b56d3ab316d2173b13b70cd82fbf89
gonzarl <gonzariquelme66@gmail.com>
2024-04-29 09:11:27 -0300

    Centrado el Mapa en Bahia Blanca, Argentina

--- app/components/Map/Map.js
+++ app/components/Map/Map.js
@@ -3,11 +3,11 @@ import 'leaflet/dist/leaflet.css'
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 
-const position = [51.505, -0.09]
+const position = [-38.7, -62.3]
 
 const Map = () => {
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: '400px', width: '100%' }}>
+    <MapContainer center={position} zoom={3} scrollWheelZoom={false} style={{ height: '400px', width: '100%' }}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
