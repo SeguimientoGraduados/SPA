0b7721e10ce416f1ec624ec274a09fd305696b5e
Dylan Hughes <dylan_21174@hotmail.com>
2024-05-01 22:54:57 -0300

    Componentes de graduados creados

---
+++ app/components/Graduados/Graduado.js
@@ -0,0 +1,34 @@
+const graduadosData = [
+  {
+    nombre: "John Michael",
+    titulo: "Manager",
+    año: "23/04/18",
+    ciudad: "Bahía Blanca",
+  },
+  {
+    nombre: "Alexa Liras",
+    titulo: "Developer",
+    año: "23/04/18",
+    ciudad: "Bahía Blanca",
+  },
+  {
+    nombre: "Laurent Perrier",
+    titulo: "Executive",
+    año: "19/09/17",
+    ciudad: "Madrid",
+  },
+  {
+    nombre: "Michael Levi",
+    titulo: "Developer",
+    año: "24/12/08",
+    ciudad: "Buenos Aires",
+  },
+  {
+    nombre: "Richard Gran",
+    titulo: "Manager",
+    año: "04/10/21",
+    ciudad: "Córdoba",
+  },
+];
+
+export default graduadosData;
---
+++ app/components/Graduados/TablaGraduadosComponent.js
@@ -0,0 +1,60 @@
+"use client";
+import { Card, Typography } from "@material-tailwind/react";
+import graduados from './Graduado';
+ 
+const TABLE_HEAD = ["Nombre", "Título", "Año", "Ciudad"];
+ 
+const TablaGraduados = ({ selectedCity }) => {
+
+  const filteredGraduates = graduados.filter((graduado) => graduado.ciudad === selectedCity);
+
+  return (
+    <Card>
+      <table className="w-full min-w-max table-auto text-center">
+        <thead>
+          <tr>
+            {TABLE_HEAD.map((head) => (
+              <th key={head} className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
+                  {head}
+                </Typography>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {filteredGraduates.map(({ nombre, titulo, año, ciudad }, index) => (
+            <tr key={nombre} className="even:bg-blue-gray-50/50">
+              <td className="p-4">
+                <Typography variant="small" color="blue-gray" className="font-normal">
+                  {nombre}
+                </Typography>
+              </td>
+              <td className="p-4">
+                <Typography variant="small" color="blue-gray" className="font-normal">
+                  {titulo}
+                </Typography>
+              </td>
+              <td className="p-4">
+                <Typography variant="small" color="blue-gray" className="font-normal">
+                  {año}
+                </Typography>
+              </td>
+              <td className="p-4">
+                <Typography variant="small" color="blue-gray" className="font-normal">
+                  {ciudad}
+                </Typography>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Card>
+  );
+};
+
+export default TablaGraduados
