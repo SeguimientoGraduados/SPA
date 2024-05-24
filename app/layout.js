8b57875ad3ce8d520451ce1b75fe2b69815af5d7
Dylan Hughes <dylanhughes@codimat.com.ar>
2024-05-24 10:22:09 -0300

    Header y Footer movidos al Layout para no re-renderizar

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,18 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header/HeaderComponent";
+import Footer from "./components/Footer/FooterComponent"
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Graduados UNS",
-  description: "Plataforma de seguimiento de los graduados de la UNS",
-};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,8 +1,6 @@
 'use client';
 import Mapa from "./components/Mapa";
 import getCiudades from "./services/ciudadesService";
-import Header from "./components/Header/HeaderComponent";
-import Footer from "./components/Footer/FooterComponent"
 import TablaGraduados from "./components/Graduados/TablaGraduadosComponent"
 import React, { useState } from "react";
 
@@ -13,21 +11,16 @@ const Home = () => {
   const [selectedCity, setSelectedCity] = useState(null);
 
   return (
-    <main>
-      <Header />
-      <section className="bg-gray-200 flex min-h-screen flex-col items-center justify-between p-24">
-
-        <Mapa
-          ciudades={ciudades}
-          selectedCity={selectedCity}
-          setSelectedCity={setSelectedCity}
-        />
-        <div className="flex justify-start w-full mt-4">
-          <TablaGraduados selectedCity={selectedCity} />
-        </div>
-      </section>
-      <Footer />
-    </main>
+    <section className="bg-gray-200 flex min-h-screen flex-col items-center justify-between p-24">
+      <Mapa
+        ciudades={ciudades}
+        selectedCity={selectedCity}
+        setSelectedCity={setSelectedCity}
+      />
+      <div className="flex justify-start w-full mt-4">
+        <TablaGraduados selectedCity={selectedCity} />
+      </div>
+    </section>
   );
 };
 
