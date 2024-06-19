6f96585c58899672ed3563301edeb91260191368
Gonzalo Martín Riquelme Ludwig <gonzalo.riquelme@assertia.com.ar>
2024-06-19 11:52:29 -0300

    Se implementa filtro por AÑO DE GRADUACIÓN

--- app/components/Mapa/Filtros.js
+++ app/components/Mapa/Filtros.js
@@ -1,18 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { IconButton, Card } from "@material-tailwind/react";
+import { IconButton, Card, Typography } from "@material-tailwind/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
 import FiltroPaisComponent from "./Filtros/FiltroPaisComponent";
 import FiltroDepartamentoComponent from "./Filtros/FiltroDepartamentoComponent";
+import FiltroAnioComponent from "./Filtros/FiltroAnioComponent";
 import graduadosService from "@/app/services/graduadosService";
 
 const Filtros = ({ onFiltrosChange }) => {
   const { obtenerValoresParaFiltrar } = graduadosService;
   const [mostrarFiltros, setMostrarFiltros] = useState(false);
+  
   const [paises, setPaises] = useState([]);
   const [departamentos, setDepartamentos] = useState([]);
+  const [anioMin, setAnioMin] = useState(null);
+  const [anioMax, setAnioMax] = useState(null);
+
   const [paisSeleccionado, setPaisSeleccionado] = useState(null);
   const [departamentoSeleccionado, setDepartamentoSeleccionado] = useState(null);
+  const [anioMinSeleccionado, setAnioMinSeleccionado] = useState(null);
+  const [anioMaxSeleccionado, setAnioMaxSeleccionado] = useState(null);
 
   const handleButtonClicked = () => {
     setMostrarFiltros(!mostrarFiltros);
@@ -20,11 +27,17 @@ const Filtros = ({ onFiltrosChange }) => {
 
   const handlePaisChange = (pais) => {
     setPaisSeleccionado(pais);
-  }
+  };
 
   const handleDepartamentoChange = (dpto) => {
     setDepartamentoSeleccionado(dpto);
-  }
+  };
+
+  const handleAnioChange = (min, max) => {
+    setAnioMinSeleccionado(min);
+    setAnioMaxSeleccionado(max);
+    console.log(min,max)
+  };
 
   useEffect(() => {
     const obtenerValores = async () => {
@@ -32,6 +45,10 @@ const Filtros = ({ onFiltrosChange }) => {
         const response = await obtenerValoresParaFiltrar();
         setPaises(response.paises);
         setDepartamentos(response.departamentos);
+        setAnioMin(response.anios.anio_min);
+        setAnioMinSeleccionado(response.anios.anio_min);
+        setAnioMax(response.anios.anio_max);
+        setAnioMaxSeleccionado(response.anios.anio_max);
       } catch (error) {
         console.error("Error al obtener los filtros:", error);
       }
@@ -48,30 +65,41 @@ const Filtros = ({ onFiltrosChange }) => {
     if (departamentoSeleccionado) {
       filtros.departamento = departamentoSeleccionado;
     }
-    
+    if (anioMinSeleccionado) {
+      filtros.anioDesde = anioMinSeleccionado;
+    }
+    if (anioMaxSeleccionado) {
+      filtros.anioHasta = anioMaxSeleccionado;
+    }
+
     onFiltrosChange(filtros);
-  }, [paisSeleccionado, departamentoSeleccionado]);
+  }, [paisSeleccionado, departamentoSeleccionado, anioMinSeleccionado, anioMaxSeleccionado]);
 
   return (
     <div style={{ position: "absolute", top: 10, left: 10, zIndex: 1000 }}>
-      <IconButton
-        className="mt-2 ml-2"
-        id="boton"
-        color="white"
-        onClick={handleButtonClicked}
-      >
-        <FontAwesomeIcon icon={faPlus} />
+      <IconButton size="lg" color="white" onClick={handleButtonClicked}>
+        <FontAwesomeIcon icon={faFilter} />
       </IconButton>
 
       {mostrarFiltros && (
-        <Card className="w-full flex flex-col gap-2 mt-2 ml-2 p-2">
+        <Card className="w-full flex flex-col gap-2 mt-2 p-2">
+          <Typography variant="paragraph" className="text-center">
+            Filtros
+          </Typography>
           <FiltroPaisComponent
             paises={paises}
             onPaisChange={(pais) => handlePaisChange(pais)}
           />
           <FiltroDepartamentoComponent
             departamentos={departamentos}
-            onDepartamentoChange={(departamento) => handleDepartamentoChange(departamento)}
+            onDepartamentoChange={(departamento) =>
+              handleDepartamentoChange(departamento)
+            }
+          />
+          <FiltroAnioComponent
+            min={anioMin}
+            max={anioMax}
+            onAnioChange={(min, max) => handleAnioChange(min, max)}
           />
         </Card>
       )}
---
+++ app/components/Mapa/Filtros/FiltroAnioComponent.js
@@ -0,0 +1,29 @@
+import { Typography } from "@material-tailwind/react";
+import { useState } from "react";
+import ReactSlider from "react-slider";
+
+const FiltroAnioComponent = ({ min, max, onAnioChange }) => {
+  const [rango, setRango] = useState([min, max]);
+  
+  const handleAnioChange = (value) => {
+    onAnioChange(value[0], value[1]);
+  };
+
+  return (
+    <div className="flex flex-col pb-12 px-3">
+      <Typography variant="small">Año de graduacion:</Typography>
+      <ReactSlider
+        className="slider"
+        thumbClassName="thumb"
+        trackClassName="track"
+        defaultValue={rango}
+        max={max}
+        min={min}
+        renderThumb={(props, state) => <Typography variant="small" {...props}>{state.valueNow}</Typography>}
+        onAfterChange={(value) => handleAnioChange(value)}
+      />
+    </div>
+  );
+};
+
+export default FiltroAnioComponent;
