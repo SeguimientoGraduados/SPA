726ad79df6c6fc585811bd620ab9293bc61118f6
Gonzalo Martín Riquelme Ludwig <gonzalo.riquelme@assertia.com.ar>
2024-07-29 11:05:39 -0300

    Fix Tooltip de Formacion

--- app/components/Graduados/FormacionTooltip.js
+++ app/components/Graduados/FormacionTooltip.js
@@ -3,14 +3,14 @@ import { faBuildingColumns } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const FormacionTooltip = ({ formacion = [] }) => {
-  return (
+  return formacion.length > 0 ? (
     <Tooltip
       className="z-50 bg-white border border-blue-gray-50"
       content={
         <div className="flex flex-col gap-2 items-center">
           {formacion.map((form, index) => (
             <Typography
-              key={index} 
+              key={index}
               variant="small"
               color="blue-gray"
               className="font-normal"
@@ -23,9 +23,14 @@ const FormacionTooltip = ({ formacion = [] }) => {
     >
       <FontAwesomeIcon
         icon={faBuildingColumns}
-        color={formacion.length > 0 ? "blue" : "gray"}
+        color="blue"
       />
     </Tooltip>
+  ) : (
+    <FontAwesomeIcon
+      icon={faBuildingColumns}
+      color="gray"
+    />
   );
 };
 
